99b3b44562bdc75be05886e6dfe6d4b344492c67
Cybermade <hamidounazim@gmail.com>
2022-11-06 22:39:46 +0100

    cleared fixed and commented

--- hits_algorithm_mpi.c
+++ hits_algorithm_mpi.c
@@ -1,7 +1,8 @@
 /*
  *
+ * @author : Cybermade
  * mpicc -o hits_algorithm_mpi hits_algorithm_mpi.c -lm
- * mpirun -np 8 ./hits_algorithm_mpi
+ * mpirun -np <nb_of_tasks> ./hits_algorithm_mpi <input_file> <nb_nodes> <nb_iterations>
  *
  */
 
@@ -9,116 +10,113 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-void read_ints(const char *file_name, int *graph, int nb_nodes)
-{
-    FILE *file = fopen(file_name, "r");
-    int i = 0;
-    int j = 0;
 
-    while (!feof(file))
-    {
-
-        fscanf(file, "%d", &i);
-        fscanf(file, "%d", &j);
-        graph[(int)i * nb_nodes + j] = 1;
-    }
-    fclose(file);
-}
+/* functions prototypes */
+void read_ints(const char *, int *, int);      /* read graph matrix from from file */
 
 int main(int argc, char *argv[])
 {
-    int numtasks, rank, len, rc;
+    /* MPI variables */
+    int numtasks, rank, len;
     char hostname[MPI_MAX_PROCESSOR_NAME];
-    char message[] = "Hello!";
-    int number[2];
-    // initialize MPI
+    
+    /* MPI initialization */
     MPI_Init(&argc, &argv);
 
-    // get number of tasks
+    /* Get the number of tasks */
     MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
 
-    // get my rank
+    /* Get the rank of the process */
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-    // this one is obvious
+    /* this one is obvious */
     MPI_Get_processor_name(hostname, &len);
-    // printf ("Number of tasks= %d My rank= %d Running on %s\n", numtasks ,rank ,hostname);
+   
+    /* Variables */
+    
+    char *nbnodes = argv[2];
+    int nb_nodes = atoi(nbnodes); /* number of nodes */
+    
 
-    int i = 0;
-    const int nb_nodes = 8;
-    double *buff_hub;
-    double *buff_autority;
-    int *graph;
-    double *hub;
-    double *autority;
-    int nb_iterations = 10;
+    int *graph = NULL; /* graph */
+    double *hub = NULL;     /* hub */
+    double *autority = NULL;     /* autority */
+    double *buff_hub = NULL;           /* buffer for hub vector (used for MPI communication) */
+    double *buff_autority = NULL;      /* buffer for autority vector (used for MPI communication) */
+    
+    char *nbiterations = argv[3];
+    int nb_iterations = atoi(nbiterations); /* number of iterations to run the algorithm */
 
-    int *sendcounts;               // array describing how many elements to send to each process
-    int *displs;                   // array describing the displacements where each segment begins
-    int rem = nb_nodes % numtasks; // elements remaining after division among processes
+    int *sendcounts;               /* array describing how many elements to send to each process */
+    int *displs;                   /* array describing the displacements where each segment begins */
+    int rem = nb_nodes % numtasks; /* elements remaining after division among processes */
     int sum = 0;
+    int begin = 0;                 /* begin index for each process */
+    double norm = 0;               /* normalization variable */
 
+    /* memory allocation */
     graph = calloc(nb_nodes * nb_nodes, sizeof(int));
     hub = calloc(nb_nodes, sizeof(double));
     autority = calloc(nb_nodes, sizeof(double));
     sendcounts = calloc(numtasks, sizeof(int));
     displs = calloc(numtasks, sizeof(int));
 
-    for (int i = 0; i < nb_nodes; i++)
-    {
-
-        autority[i] = 1;
-        hub[i] = 1;
-    }
-
+    /* read graph from file (we assume that the root process has the graph and it is broadcasted to all processes) */
     if (rank == 0)
     {
-        read_ints("test2.txt", graph, nb_nodes);
+        /* read graph from file */
+        read_ints(argv[1], graph, nb_nodes); /* read graph from file */
+        /* initialize hub and autority vectors */
         for (int i = 0; i < nb_nodes; i++)
         {
             hub[i] = 1;
             autority[i] = 1;
         }
+        /* calculate sendcounts and displs */
+        for (int i = 0; i < numtasks; i++)
+        {
+            sendcounts[i] = nb_nodes / numtasks;
+            if (rem > 0)
+            {
+                sendcounts[i]++;
+                rem--;
+            }
+            displs[i] = sum;
+            sum += sendcounts[i];
+        }
+        /* create or clear the output file */
+        fclose(fopen("result_hits_mpi.txt", "w"));
     }
-
+    /* broadcast graph, hub, autority to all processes */
     MPI_Bcast(graph, nb_nodes * nb_nodes, MPI_INT, 0, MPI_COMM_WORLD);
-
-    printf("i am %d\n", rank);
-    for (int i = 0; i < nb_nodes; i++)
-    {
-        for (int j = 0; j < nb_nodes; j++)
-        {
-            printf("%d ", graph[i * nb_nodes + j]);
-        }
-        printf("\n");
-    }
-    printf("\n\n\n");
-
-    for (int i = 0; i < numtasks; i++)
-    {
-        sendcounts[i] = nb_nodes / numtasks;
-        if (rem > 0)
-        {
-            sendcounts[i]++;
-            rem--;
-        }
-        displs[i] = sum;
-        sum += sendcounts[i];
-    }
-
-    buff_hub = calloc(sendcounts[rank], sizeof(int));
-    buff_autority = calloc(sendcounts[rank], sizeof(int));
-    
     MPI_Bcast(hub, nb_nodes, MPI_DOUBLE, 0, MPI_COMM_WORLD);
     MPI_Bcast(autority, nb_nodes, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
-    double norm = 0;
+    
+    
 
-    int begin = displs[rank];
+    /* broadcast sendcounts and displs to all processes */
+    MPI_Bcast(sendcounts, numtasks, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(displs, numtasks, MPI_INT, 0, MPI_COMM_WORLD);
+
+    /* allocate memory for buffers */
+    buff_hub = calloc(sendcounts[rank], sizeof(double));
+    buff_autority = calloc(sendcounts[rank], sizeof(double));
+
+    /* calculate begin index for each process */
+    begin = displs[rank];
+
+    
+    
+    /* begin computation */
     for (int k = 0; k < nb_iterations; k++)
-    {
+    {   
+        
+        
+        /* calculate autority vector */
         for (int i = 0; i < sendcounts[rank]; i++)
         {
+            
             buff_autority[i] = 0;
             for (int j = 0; j < nb_nodes; j++)
             {
@@ -128,7 +126,10 @@ int main(int argc, char *argv[])
                 }
             }
         }
+        /* gather autority vector */
         MPI_Gatherv(buff_autority, sendcounts[rank], MPI_DOUBLE, autority, sendcounts, displs, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        
+        /* root calculate sum of autority vector */
         if (rank == 0)
         {
             norm = 0;
@@ -139,14 +140,20 @@ int main(int argc, char *argv[])
             norm = sqrt(norm);
                
         }
-
+        /* broadcast normalize variable to all processes */
         MPI_Bcast(&norm, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        /* each process normalize its part of the autority vector */
         for(int i = 0; i<sendcounts[rank]; i++){
             buff_autority[i] = buff_autority[i]/norm;
         }
+        /* print hub and autority vectors */
+        
+        /* gather autority vector */
         MPI_Gatherv(buff_autority, sendcounts[rank], MPI_DOUBLE, autority, sendcounts, displs, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        /* broadcast autority vector to all processes */
         MPI_Bcast(autority, nb_nodes, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
+        /* calculate hub vector */
         for (int i = 0; i < sendcounts[rank]; i++)
         {
             buff_hub[i] = 0;
@@ -158,9 +165,10 @@ int main(int argc, char *argv[])
                 }
             }
         }
-        
+        /* gather hub vector */
         MPI_Gatherv(buff_hub, sendcounts[rank], MPI_DOUBLE, hub, sendcounts, displs, MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
+        /* root calculate sum of hub vector */
         if (rank == 0)
         {
             norm = 0;
@@ -172,33 +180,73 @@ int main(int argc, char *argv[])
             
         }
         
+        /* broadcast normalize variable to all processes */
         MPI_Bcast(&norm, 1, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        /* each process normalize its part of the hub vector */
         for(int i = 0; i<sendcounts[rank]; i++){
             buff_hub[i] = buff_hub[i]/norm;
         }
+        /* gather hub vector */
         MPI_Gatherv(buff_hub, sendcounts[rank], MPI_DOUBLE, hub, sendcounts, displs, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        /* broadcast hub vector to all processes */
         MPI_Bcast(hub, nb_nodes, MPI_DOUBLE, 0, MPI_COMM_WORLD);
         
         if (rank == 0)
         {
+            FILE *fptr;
+            fptr = fopen("result_hits_mpi.txt", "a");
             printf("Iteration NB : %d\nHub :\t\tAutority :\n",k);
+            fprintf(fptr, "Iteration NB : %d\nHub :\t\tAutority :\n",k);
             for (int i = 0; i < nb_nodes; i++)
             {
                 printf("%d -> %.5f , %d -> %.5f\n", i, hub[i], i, autority[i]);
+                fprintf(fptr, "%d -> %.5f , %d -> %.5f\n", i, hub[i], i, autority[i]);
             }
             printf("\n");
+            fprintf(fptr, "\n");
+            
+            fclose(fptr);
         }
     }
     
-
+    /* free memory */
     free(graph);
+    graph = NULL;
     free(hub);
+    hub = NULL;
     free(autority);
+    autority = NULL;
     free(sendcounts);
+    sendcounts = NULL;
     free(displs);
+    displs = NULL;
     free(buff_hub);
+    buff_hub = NULL;
     free(buff_autority);
+    buff_autority = NULL;
 
-    // done with MPI
+    /* finalize MPI */
     MPI_Finalize();
+}
+void read_ints(const char *file_name, int *graph, int nb_nodes)
+{
+    if (file_name == NULL)
+    {
+        printf("Error: file name is NULL\n");
+        exit(1);
+    }
+    /* open file */
+    FILE *file = fopen(file_name, "r");
+    int i = 0;
+    int j = 0;
+
+    while (!feof(file))
+    {
+
+        fscanf(file, "%d", &i);
+        fscanf(file, "%d", &j);
+
+        graph[(int)i * nb_nodes + j] = 1;
+    }
+    fclose(file);
 }
